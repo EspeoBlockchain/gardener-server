c4926ef90f74b6ab1f9a79db82d9c894e5a6e6f9
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2018-09-18 14:11:44 +0200

    added status page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').load();
+require('./src/utils/statusPage');
 const RequestProcessor = require('./src/RequestProcessor');
 
 const requestProcessor = new RequestProcessor(process.env.ORACLE_ADDRESS);
---
+++ src/utils/statusPage.js
@@ -0,0 +1,15 @@
+/* eslint no-console: 0 */
+const express = require('express');
+const config = require('config');
+
+const app = express();
+const port = config.get('port');
+
+
+const okResponse = {
+  status: 'alive',
+};
+
+app.get('/status', (req, res) => res.send(okResponse));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
