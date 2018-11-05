0d3328206ff1885623e17b1a2aad0bbc8872a19d
Wojciech Kalinski <wojciech.kalinski@espeo.eu>
2018-11-05 17:01:37 +0100

    Initial commit for adding mongo in oracle-server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 require('dotenv').load();
 require('./src/utils/statusPage');
+const mongoose = require('mongoose');
 const RequestProcessor = require('./src/RequestProcessor');
 
 const requestProcessor = new RequestProcessor(process.env.ORACLE_ADDRESS);
 requestProcessor.listen();
+
+mongoose.connect('mongodb://mongo:27017/oracle-server',
+  { useNewUrlParser: true })
+  .then(() => console.log('mongodb connected'))
+  .catch(err => console.log(err));
+
+app
