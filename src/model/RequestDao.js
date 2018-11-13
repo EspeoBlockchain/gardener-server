7988b44e30c61fec71c803bf81591dcc7b17d366
Patryk Okuniewicz <patryk.okuniewicz@espeo.eu>
2018-11-13 13:35:22 +0100

    Add MongoDB support

---
+++ src/model/RequestDao.js
@@ -0,0 +1,34 @@
+/* eslint-disable class-methods-use-this */
+const mongoose = require('mongoose');
+const RequestSchema = require('./requestSchema');
+
+class RequestDao {
+  saveRequest(url, ended) {
+    const newRequest = RequestSchema({
+      _id: new mongoose.Types.ObjectId(),
+      url,
+      ended,
+    });
+
+    newRequest.save((err) => {
+      if (err) throw err;
+
+      console.log('Request successfully saved');
+    });
+  }
+
+  findSingleRequestUrlByEndDate(end) {
+    return RequestSchema.find({ ended: { $lte: end } }, (err, requestObject) => {
+      if (err) throw err;
+
+      if (requestObject.status && requestObject.request.length > 0) {
+        const request = requestObject.request[0];
+        console.log(`Found request: ${request}`);
+        return request.url;
+      }
+      return '';
+    });
+  }
+}
+
+module.exports = RequestDao;
