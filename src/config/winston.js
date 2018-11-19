a9c178b1395cda7bfde0991519d4865e9e5bfe16
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2018-11-19 10:12:19 +0100

    temporarly disable silent in winston to check pipelines

--- src/config/winston.js
+++ src/config/winston.js
@@ -26,7 +26,7 @@ const logger = createLogger({
   ),
   transports: [new transports.Console(options.console)],
   exitOnError: false,
-  silent: env === 'test',
+  // silent: env === 'test',
 });
 
 module.exports = logger;
