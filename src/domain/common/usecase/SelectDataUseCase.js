bdcbd801b1b0cc0cc53af6ca45ce11cc308411a4
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2018-12-06 13:14:53 +0100

    OSO-49 changed SelectDataUseCase

--- src/domain/common/usecase/SelectDataUseCase.js
+++ src/domain/common/usecase/SelectDataUseCase.js
@@ -1,14 +1,11 @@
 class SelectDataUseCase {
-  constructor(dataSelectorFinder, requestRepository, responseRepository, logger) {
+  constructor(dataSelectorFinder, responseRepository, logger) {
     this.dataSelectorFinder = dataSelectorFinder;
-    this.requestRepository = requestRepository;
     this.responseRepository = responseRepository;
     this.logger = logger;
   }
 
-  async selectFromRawData(response) {
-    const request = await this.requestRepository.find(response.requestId);
-
+  async selectFromRawData(request, response) {
     const contentType = request.getContentType();
     const path = request.getSelectionPath();
     const selectedData = this._selectData(contentType, path);
@@ -18,9 +15,9 @@ class SelectDataUseCase {
      `);
 
     response.addSelectedData(selectedData);
-    response.state.markAsFinished();
     this.responseRepository.save(response);
-    this.logger.info(`Response for request ${response.requestId} marked as finished`);
+
+    return response;
   }
 
   _selectData(fetchedData, contentType, path) {
--- src/domain/common/usecase/SelectDataUseCase.spec.js
+++ src/domain/common/usecase/SelectDataUseCase.spec.js
@@ -2,6 +2,7 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 const SelectDataUseCase = require('./SelectDataUseCase');
 const Request = require('../../request/Request');
+const RequestStateEnum = require('../../request/RequestStateEnum');
 const Response = require('../../response/Response');
 
 describe('SelectDataUseCase', () => {
@@ -13,11 +14,6 @@ describe('SelectDataUseCase', () => {
     };
   };
 
-  const crawler = () => ({
-    // eslint-disable-next-line no-unused-vars
-    fetch: url => JSON.stringify({ key1: 'value1' }),
-  });
-
   const logger = () => {
     const logs = [];
     return {
@@ -26,18 +22,20 @@ describe('SelectDataUseCase', () => {
     };
   };
 
-  it('should select appropriate data from fetch one', async () => {
-    expect(0).to.equal(1);
+  const finder = () => ({
+    find: () => ({ select: () => 'selectedData' }),
+  });
 
+  it('should select appropriate data from fetch one', async () => {
     // given
-    // const request = new Request('1', 'json(http://example.com).key1', Date.now());
-    // const sut = new FetchDataUseCase(crawler(), repository(), logger());
-    // // when
-    // const response = await sut.fetchDataForRequest(request);
-    // // then
-    // expect(response.requestId).to.equal('1');
-    // expect(response.fetchedData).to.equal(JSON.stringify({ key1: 'value1' }));
-    // expect(sut.responseRepository.list()).to.have.lengthOf(1);
-    // expect(sut.logger.list()).to.have.lengthOf(2);
+    const sut = new SelectDataUseCase(finder(), repository(), logger());
+    const request = new Request('id', 'json(http://xample.com).key1', Date.now(), RequestStateEnum.PROCESSED);
+    const response = new Response('id');
+    response.addFetchedData('fetchedData');
+    // when
+    const responseWithSelectedData = await sut.selectFromRawData(request, response);
+    // then
+    expect(responseWithSelectedData.selectedData).to.equal('selectedData');
+    expect(sut.logger.list()).to.have.lengthOf(1);
   });
 });
