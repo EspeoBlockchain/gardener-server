a6380f8093211f12857ad239f2115740a009f90e
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2018-12-06 13:15:35 +0100

    OSO-49 added tests for MarkValidRequestsAsReadyUseCase

---
+++ src/domain/request/usecase/MarkValidRequestsAsReadyUseCase.spec.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const Request = require('../Request');
+const RequestStateEnum = require('../RequestStateEnum');
+const MarkValidRequestsAsReadyUseCase = require('./MarkValidRequestsAsReadyUseCase');
+
+describe('CreateRequestUseCase', () => {
+  const repository = () => {
+    const requests = [];
+    return {
+      save: req => requests.push(req),
+      list: () => requests,
+      getScheduledRequestsWithValidFromBeforeNow: () => requests,
+      getById: id => requests[0],
+    };
+  };
+
+  const logger = () => {
+    const logs = [];
+    return {
+      info: log => logs.push(log),
+      list: () => logs,
+    };
+  };
+
+  it('should mark all scheduled requests with validFrom less than now as ready', () => {
+    // given
+    const requestRepository = repository();
+    requestRepository.save(new Request('1', 'url', Date.now(), RequestStateEnum.SCHEDULED));
+
+    const sut = new MarkValidRequestsAsReadyUseCase(requestRepository, logger());
+
+    // when
+    sut.markValidRequestsAsReady();
+    // then
+    expect(requestRepository.getById('1').state.name).to.equal(RequestStateEnum.READY);
+    expect(sut.logger.list().length).to.equal(1);
+  });
+});
