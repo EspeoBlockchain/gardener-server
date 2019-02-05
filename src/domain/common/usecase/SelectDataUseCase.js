66338850499730392af5898b4be99861185fd5db
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2019-02-05 16:38:13 +0100

    [#18] added InvalidSelectorDataError(4000) and NoMatchingElementsFoundError(4004)

--- src/domain/common/usecase/SelectDataUseCase.js
+++ src/domain/common/usecase/SelectDataUseCase.js
@@ -1,3 +1,5 @@
+const { NoMatchingElementsFoundError, InvalidSelectorDataError } = require('../utils/error');
+
 class SelectDataUseCase {
   constructor(dataSelectorFinder, logger) {
     this.dataSelectorFinder = dataSelectorFinder;
@@ -5,7 +7,19 @@ class SelectDataUseCase {
   }
 
   async selectFromRawData(fetchedData, contentType, path) {
-    const selectedData = this.dataSelectorFinder.find(contentType).select(fetchedData, path);
+    let selectedData;
+
+    try {
+      selectedData = await this.dataSelectorFinder.find(contentType).select(fetchedData, path);
+    } catch (e) {
+      throw new InvalidSelectorDataError(e.toString());
+    }
+
+
+    if (selectedData === null) {
+      throw new NoMatchingElementsFoundError('No matched elements');
+    }
+
     this.logger.info(`Data selected [selectedData=${selectedData},fetchedData=${fetchedData},contentType=${contentType},path=${path}`);
 
     return selectedData;
--- src/domain/common/usecase/SelectDataUseCase.spec.js
+++ src/domain/common/usecase/SelectDataUseCase.spec.js
@@ -1,20 +1,39 @@
 const { describe, it } = require('mocha');
-const { expect } = require('chai');
+const { expect } = require('chai').use(require('chai-as-promised'));;
 const SelectDataUseCase = require('./SelectDataUseCase');
 const { Logger } = require('../utils/TestMocks');
 
 describe('SelectDataUseCase', () => {
-  const finder = () => ({
-    find: () => ({ select: () => Promise.resolve('selectedData') }),
-  });
-
   it('should select appropriate data', async () => {
     // given
+    const finder = () => ({
+      find: () => ({ select: () => Promise.resolve('selectedData') }),
+    });
     const sut = new SelectDataUseCase(finder(), new Logger());
     // when
     const selectedData = await sut.selectFromRawData('fetchedData', 'json', '.key1');
     // then
     expect(selectedData).to.equal('selectedData');
     expect(sut.logger.list()).to.have.lengthOf(1);
+  });
+
+  it('should throw NoMatchingElementsFoundError when no data matched', async () => {
+    // given
+    const finder = () => ({
+      find: () => ({ select: () => Promise.resolve(null) }),
+    });
+    const sut = new SelectDataUseCase(finder(), new Logger());
+    // when, then
+    return expect(sut.selectFromRawData('fetchedData', 'json', '.key1')).to.be.rejected;
+  });
+
+  it('should throw InvalidSelectorDataError selector throw any error', async () => {
+    // given
+    const finder = () => ({
+      find: () => ({ select: () => Promise.reject() }),
+    });
+    const sut = new SelectDataUseCase(finder(), new Logger());
+    // when, then
+    return expect(sut.selectFromRawData('fetchedData', 'json', '.key1')).to.be.rejected;
   });
 });
