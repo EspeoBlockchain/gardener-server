571c5577431cadb91a0063a8903101ddc2458d10
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki@espeo.eu>
2019-07-17 12:36:58 +0200

    successfully integrated sgx lib using node-ffi

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
 FROM sebvaucher/sgx-base:sgx_2.5
 
-# Add SGX SDK libs to source
-RUN /bin/bash -c "source /opt/intel/sgxsdk/environment"
+# Add SGX SDK libs to source. It does what 'source /opt/intel/sgxsdk/environment' does
+ENV SGX_SDK /opt/intel/sgxsdk
+ENV PATH=$SGX_SDK/bin:$SGX_SDK/bin/x64:$PATH \
+    PKG_CONFIG_PATH=$SGX_SDK/pkgconfig:$PKG_CONFIG_PATH \
+    LD_LIBRARY_PATH=$SGX_SDK/sdk_libs:$LD_LIBRARY_PATH
 
-# Install nodejs
+# Install nodejs dependencies
 RUN apt-get update -q -q && \
-    apt-get install libssl1.0-dev nodejs nodejs-dev node-gyp npm --yes
+    apt-get install curl libssl1.0-dev --yes
+
+# Install nodejs v11
+RUN curl -sL https://deb.nodesource.com/setup_11.x | bash -
+RUN apt-get install -y nodejs
+RUN npm install npm --global
 
 # Create app directory
 WORKDIR /usr/src/app
--- src/application/dataFetcher/random/RandomSgxDataFetcherAdapter.js
+++ src/application/dataFetcher/random/RandomSgxDataFetcherAdapter.js
@@ -1,29 +1,51 @@
 const ffi = require('ffi');
 const ref = require('ref');
 
+const RequestUrlParser = require('../../../domain/request/RequestUrlParser');
+
 const intType = ref.types.int;
 const longType = ref.types.long;
-const longPtr = ref.refType('long');
+const longPtrType = ref.refType(ref.types.long);
 
 class RandomSgxDataFetcherAdapter {
   constructor(logger) {
     this.logger = logger;
+    this.generateRandom = this._loadFunctionFromSgxLib(
+        'generateRandom',
+        intType,
+        [longType, longType, longPtrType]
+    );
   }
 
-  async fetch(min, max) {
-    this.logger.info('fetching SGX shiet');
-    const sgxLib = ffi.DynamicLibrary('/usr/src/app/libs/EnclaveWrapper.so', {
-      'generateRandom': [intType, [longType, longType, longPtr]],
-    });
-    //const sgxLib = ffi.DynamicLibrary('/usr/src/app/libs/EnclaveWrapper.so');
+  async fetch(request) {
+    const min = RequestUrlParser.resolveLeftSideBound(request.url);
+    const max = RequestUrlParser.resolveRightSideBound(request.url);
 
-    const randomPtr = ref.alloc(longType);
-    if (sgxLib.generateRandom(min, max, randomPtr) === -1) {
+    const longPtrAlloc = ref.alloc(longType);
+
+    if (await this.generateRandom(await this._numberToLong(min), await this._numberToLong(max), longPtrAlloc) === -1) {
       throw new Error('SGX call failed');
     }
-    this.logger.info('fetched SGX shiet');
 
-    return randomPtr.deref();
+    return longPtrAlloc.deref().toString();
+  }
+
+  _loadFunctionFromSgxLib(name, returnType, params) {
+    const mode = ffi.DynamicLibrary.FLAGS.RTLD_NOW | ffi.DynamicLibrary.FLAGS.RTLD_GLOBAL;
+
+    ffi.DynamicLibrary('libsgx_urts.so', mode);
+    ffi.DynamicLibrary('Enclave.signed.so', mode);
+    const sgxLib = ffi.DynamicLibrary('EnclaveWrapper.so', mode);
+
+    return ffi.ForeignFunction(sgxLib.get(name), returnType, params);
+  }
+
+  async _numberToLong(number) {
+    let buf = Buffer.alloc(4);
+    buf.writeInt64LE(number, 0);
+    buf.type = ref.types.long;
+
+    return buf.deref();
   }
 }
 
