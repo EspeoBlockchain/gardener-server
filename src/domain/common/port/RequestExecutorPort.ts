1097e104d203048821dd47f8e51f90695a4c8b0d
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki@espeo.eu>
2019-07-18 14:43:25 +0200

    using request executors

--- src/domain/common/port/RequestExecutorPort.ts
+++ src/domain/common/port/RequestExecutorPort.ts
@@ -1,3 +1,6 @@
+import Request from '@core/domain/request/Request';
+import Response from '@core/domain/response/Response';
+
 export default interface RequestExecutorPort {
-    execute(request: Request);
+    execute(request: Request, response: Response): Response;
 }
