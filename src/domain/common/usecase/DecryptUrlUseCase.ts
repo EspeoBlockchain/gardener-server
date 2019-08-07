327a5987d319419e3df2f9ca9bf67026ce6d9160
Krzysztof Spisak-Spisacki <krzysztof.spisak.spisacki@espeo.eu>
2019-08-07 17:11:44 +0200

    stylistic changes as per CR

--- src/domain/common/usecase/DecryptUrlUseCase.ts
+++ src/domain/common/usecase/DecryptUrlUseCase.ts
@@ -6,32 +6,31 @@ class DecryptUrlUseCase {
   private typeRegex = new RegExp(/(encrypted\(.*?\))/g);
 
   constructor(private readonly privateKey: string) {
-    this.privateKey = privateKey;
   }
 
   async decrypt(url: string): Promise<string> {
-    let result = url;
-
-    if (this.typeRegex.test(url)) {
-        for (const group of url.match(this.typeRegex)) {
-            const encrypted = group.substr(group.indexOf('(') + 1, group.length - 'encrypted()'.length);
-
-            let decrypted: string;
-            try {
-                decrypted = await EthCrypto.decryptWithPrivateKey(
-                    this.privateKey,
-                    EthCrypto.cipher.parse(encrypted),
-                );
-            } catch (e) {
-                throw new InvalidEncryptionError('Invalid encryption data. Expected a stringified object ' +
-                    '{iv, ephemPublicKey, ciphertext, mac} encrypted with gardener public key')
-                ;
-            }
-            result = result.replace(group, decrypted);
-        }
+    if (!this.typeRegex.test(url)) {
+        return url;
     }
 
-    return result;
+    for (const group of url.match(this.typeRegex)) {
+        const encrypted = group.substr(group.indexOf('(') + 1, group.length - 'encrypted()'.length);
+
+        let decrypted: string;
+        try {
+            decrypted = await EthCrypto.decryptWithPrivateKey(
+                this.privateKey,
+                EthCrypto.cipher.parse(encrypted),
+            );
+        } catch (e) {
+            throw new InvalidEncryptionError('Invalid encryption data. Expected a stringified object ' +
+                '{iv, ephemPublicKey, ciphertext, mac} encrypted with gardener public key')
+            ;
+        }
+        url = url.replace(group, decrypted);
+    }
+
+    return url;
   }
 }
 
